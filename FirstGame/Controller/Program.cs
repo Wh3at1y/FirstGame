a986137702cce4f334f79a2e2dffb112aa047960
Sam Montoya <smon7958@740s-pl-332.local>
2016-05-10 11:47:34 -0600

    Renamed and new NameSpace

--- FirstGame/Controller/Program.cs
+++ FirstGame/Controller/Program.cs
@@ -13,7 +13,7 @@ using UIKit;
 #endif
 #endregion
 
-namespace FirstGame
+namespace FirstGame.Controller
 {
 	#if __IOS__ || __TVOS__
     [Register("AppDelegate")]
@@ -23,11 +23,11 @@ namespace FirstGame
 	static class Program
     #endif
     {
-		private static Game1 game;
+		private static FirstGame game;
 
 		internal static void RunGame ()
 		{
-			game = new Game1 ();
+			game = new FirstGame ();
 			game.Run ();
 			#if !__IOS__  && !__TVOS__
 			game.Dispose ();
